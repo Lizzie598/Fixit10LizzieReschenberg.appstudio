4978532d246bcc31b812ff3a0103e818d9ed90fa
Lizzie <ekr74869@creighton.edu>
2019-11-03 13:03:50 -0600

    chan

--- forms/customerSelect/customerSelect.js
+++ forms/customerSelect/customerSelect.js
@@ -4,3 +4,7 @@ customerSelect.onshow=function(){
   let query = customerSelect = "SELECT name FROM customer WHERE state = " + '"' + userState + '"';
   req1 = Ajax("https://ormond.creighton.edu/courses/375/ajax-connection.php", "POST", "host=ormond.creighton.edu&user=ekr74869&pass=Boolarina18%&database=ekr74869&query=" + query)
 }
+
+btnCorpName.onclick=function(){
+  
+}
