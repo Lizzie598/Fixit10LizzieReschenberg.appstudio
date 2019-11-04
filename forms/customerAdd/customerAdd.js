f01ab7195fd3ec7d4c8a6ccd7f39975f2f201241
Lizzie <ekr74869@creighton.edu>
2019-11-03 18:19:48 -0600

    g

--- forms/customerAdd/customerAdd.js
+++ forms/customerAdd/customerAdd.js
@@ -1,7 +1,7 @@
 
 customerAdd.onshow=function(){
   let userState = txtaState.value
-  let query = customerSelect = "SELECT name FROM customer WHERE state = " + '"' + userState + '"';
+  let queryAdd = customerSelect = "INSERT INTO customer VALUES("Jesse Antiques", "1113 F St", "Omaha", "NE", "68178")";
   req1 = Ajax("https://ormond.creighton.edu/courses/375/ajax-connection.php", "POST", "host=ormond.creighton.edu&user=ekr74869&pass=Boolarina18%&database=ekr74869&query=" + query)
   if (req1.status == 200) {
     if (req1.status == 500)
--- forms/customerDelete/customerDelete.js
+++ forms/customerDelete/customerDelete.js
@@ -1,29 +1,13 @@
-let customers = []
 
 btnDeleteCus.onclick=function(){
-    let nameDel = inptName.value 
-    let returned = false
-    for (i = 0; i <= customers.length - 1; i++) {
-        if (nameDel == customers[i][1])
-            returned = true
- }
-    if (returned == false) 
-       NSB.MsgBox("That name is not available since it is not in the database.")
-    else if (returned == true) {
-    let queryDelete = customerSelect = "DELETE FROM customer WHERE name = " + '"' + delName + '"'
+  let userState = txtaState.value
+  let queryDelete = customerSelect = "DELETE FROM customer WHERE name = " + '"' + delName + '"'
   req1 = Ajax("https://ormond.creighton.edu/courses/375/ajax-connection.php", "POST", "host=ormond.creighton.edu&user=ekr74869&pass=Boolarina18%&database=ekr74869&query=" + query)
   if (req1.status == 200) {
-     if (req1.responseText == 500) 
-            NSB.MsgBox("The following company has been deleted: " + nameDel)
-            var message = ""
-        for (i = 0; i <= results.length - 1; i++)
-            message = message + results[i][0] + "\n"
-        txtaDelete.value = message
-        else
-            NSB.MsgBox("The follow company was not deleted from the database: " + nameDel + ".")
-      else {
-
-        NSB.MsgBox("Error: " + req1.status);
-      }  
-  } 
+    if (req1.status == 500)
+     alert("It's been successfully deleted!")
+   else 
+     alert("Error: It wasn't able to be deleted.")
+   else
+     alert("Error: " + req1.status)
 }
