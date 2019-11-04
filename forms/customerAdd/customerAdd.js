8dad2a3c311420455bae502af930ba75622ea52c
Lizzie <ekr74869@creighton.edu>
2019-11-03 18:05:15 -0600

    cha

--- forms/customerAdd/customerAdd.js
+++ forms/customerAdd/customerAdd.js
@@ -1,21 +1,13 @@
 
-btnAddName.onclick=function(){
-    let customerName = inptCustName.value
-    let queryAdd = "INSERT INTO customer VALUES("Jesse Antiques", "1113 F St", "Omaha", "NE", "68178")"
-
-    req1 = Ajax("https://ormond.creighton.edu/courses/375/ajax-connection.php", "POST", "host=ormond.creighton.edu&user=ekr74869&pass=Boolarina18%&database=ekr74869&query=" + query)
-  if (req1.status == 200) { 
-
-        results = JSON.parse(req1.responseText)
-    if (results.length == 0)
-        NSB.MsgBox("Error: there are no names like that.") 
-    else {        
-        var message = ""
-        for (i = 0; i <= results.length - 1; i++)
-            message = message + results[i][1] + "\n"
-        txtaReturn.value = message
-      } 
-    else
-
-        NSB.MsgBox("Error code: " + req1.status)
+customerAdd.onshow=function(){
+  let userState = txtaState.value
+  let query = customerSelect = "SELECT name FROM customer WHERE state = " + '"' + userState + '"';
+  req1 = Ajax("https://ormond.creighton.edu/courses/375/ajax-connection.php", "POST", "host=ormond.creighton.edu&user=ekr74869&pass=Boolarina18%&database=ekr74869&query=" + query)
+  if (req1.status == 200) {
+    if (req1.status == 500)
+     alert("It's been successfully added!")
+   else 
+     alert("Error: It wasn't able to be added.")
+   else
+     alert("Error: " + req1.status)
 }
--- forms/customerSelect/customerSelect.js
+++ forms/customerSelect/customerSelect.js
@@ -1,4 +1,20 @@
 
+customerSelect.onshow=function(){
+  let userState = txtaState.value
+  let query = customerSelect = "SELECT name FROM customer WHERE state = " + '"' + userState + '"';
+  req1 = Ajax("https://ormond.creighton.edu/courses/375/ajax-connection.php", "POST", "host=ormond.creighton.edu&user=ekr74869&pass=Boolarina18%&database=ekr74869&query=" + query)
+  if (req1.status == 200) {
+      results = JSON.parse(req1.responseText)  
+        console.log("The parsed JSON is " + results)
+    let message = ""
+        for (i = 0; i <= results.length - 1; i++)
+            message = message + results[i][0] + "\n"
+        txtaState.value = message
+} else
+
+        NSB.MsgBox("Error code: " + req1.status)
+}
+
 btnCorpName.onclick=function(){
   let userState = txtaState.value
   let query = customerSelect = "SELECT name FROM customer WHERE state = " + '"' + userState + '"';
@@ -17,4 +33,6 @@ btnCorpName.onclick=function(){
 
         NSB.MsgBox("Error code: " + req1.status)
 }
+
+
 
